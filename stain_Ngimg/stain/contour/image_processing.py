f8d2f914194005f766e71c33b550a9573c34fc8a
mm25712423 <mm25712423@gmail.com>
2018-01-05 12:34:09 +0800

    ImageFilter

---
+++ stain_Ngimg/stain/contour/image_processing.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+from PIL import Image
+from PIL import ImageFilter
+import glob
+
+
+'''
+for dirname, dirnames, filenames in os.walk(os.getcwd()):
+
+    for filename in filenames:
+        file_path = os.path.join(dirname, filename)
+        print (file_path)
+'''
+'''
+for dirname, dirnames, filenames in os.walk(os.getcwd()):
+
+    # print path to all filenames.
+    for filename in filenames:
+        file_path = os.path.join(dirname, filename)
+        image = Image.open(file_path)
+        image.filter(ImageFilter.EDGE_ENHANCE_MORE).show()
+'''
+
+for filename in glob.glob(os.getcwd() + '/*/*.jpg'):
+    image = Image.open(filename)
+    print (filename)
+    image.filter(ImageFilter.CONTOUR).save(filename, 'jpeg')
+    #image.save(filename, 'jpeg')
